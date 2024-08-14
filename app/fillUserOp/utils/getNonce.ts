ccd482a376745440a1847dfdd7639a9375c7a4b2
Lukas Rosario <36800180+lukasrosario@users.noreply.github.com>
2024-08-14 03:32:16 -0400

    random nonce key

--- app/fillUserOp/utils/getNonce.ts
+++ app/fillUserOp/utils/getNonce.ts
@@ -2,11 +2,16 @@ import { entrypointAbi, entrypointAddress } from "@/app/abi/entrypoint";
 import { publicClient } from "@/app/clients";
 import { Address } from "viem";
 
+const MIN_NONCE_KEY = 69;
+const MAX_NONCE_KEY = 420;
+
 export function getNonce(address: Address): Promise<bigint> {
+  const nonceKey = Math.floor(Math.random() * MAX_NONCE_KEY) + MIN_NONCE_KEY;
+
   return publicClient.readContract({
     address: entrypointAddress,
     abi: entrypointAbi,
     functionName: "getNonce",
-    args: [address, BigInt(0)],
+    args: [address, BigInt(nonceKey)],
   });
 }
